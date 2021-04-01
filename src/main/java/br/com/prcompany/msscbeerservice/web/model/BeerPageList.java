81a77e069f87a1e57fdf4b9a6091385e98f9c6d2
Paulo Roberto <paulorobertosp@gmail.com>
2021-04-01 10:20:21 -0300

    Adding web model, closes #2

---
+++ src/main/java/br/com/prcompany/msscbeerservice/web/model/BeerPageList.java
@@ -0,0 +1,18 @@
+package br.com.prcompany.msscbeerservice.web.model;
+
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPageList extends PageImpl<BeerDTO> {
+
+    public BeerPageList(List<BeerDTO> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPageList(List<BeerDTO> content) {
+        super(content);
+    }
+}
---
+++ src/main/java/br/com/prcompany/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package br.com.prcompany.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
